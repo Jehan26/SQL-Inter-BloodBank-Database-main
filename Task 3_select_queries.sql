1726c3334c36c162e4feea23973508f732135cca
Jehan B <jehan2628@gmail.com>
2025-07-05 10:51:50 +0530

    Update Task 3_select_queries.sql

--- Task 3_select_queries.sql
+++ Task 3_select_queries.sql
@@ -1,31 +1,34 @@
-1. Select All Students:
-SELECT * FROM Student;
+-- Task 3: Extracting Data from Blood Bank Database
 
-2. Select Only Student Names and Emails:
-SELECT Name, Email FROM Student;
+-- 1. Select all donors
+SELECT * FROM Donors;
 
-3. Select Students from Computer Science Department (DeptID = 1):
-SELECT * FROM Student WHERE DeptID = 1;
+-- 2. Get donors who are under 30
+SELECT Name, Age, Gender FROM Donors
+WHERE Age < 30;
 
-4. Use LIKE to Find Names Starting with "K":
-SELECT * FROM Student WHERE Name LIKE 'K%';
+-- 3. List all female receivers sorted by age descending
+SELECT Name, Age, Gender FROM Receivers
+WHERE Gender = 'Female'
+ORDER BY Age DESC;
 
- 5. Use BETWEEN to Filter Enrollments by Date:
-SELECT * FROM Enrollment
-WHERE EnrollmentDate BETWEEN '2025-06-24' AND '2025-06-26';
+-- 4. Show top 5 most recent blood donations
+SELECT d.ID, d.Name AS DonorName, bd.Date, bd.Quantity
+FROM BloodDonations bd
+JOIN Donors d ON bd.DonorID = d.ID
+ORDER BY bd.Date DESC
+LIMIT 5;
 
-6. Combine Conditions Using AND:
-SELECT * FROM Course WHERE Credits > 3 AND DeptID = 1;
+-- 5. Get all requests of blood type 'A+'
+SELECT r.Name AS ReceiverName, bt.Type AS BloodType, br.Quantity, br.Date
+FROM BloodRequests br
+JOIN Receivers r ON br.ReceiverID = r.ID
+JOIN BloodTypes bt ON r.BloodTypeID = bt.ID
+WHERE bt.Type = 'A+';
 
-7. Use OR to Find Students from Dept 1 or Dept 3:
-SELECT * FROM Student WHERE DeptID = 1 OR DeptID = 3;
-
-8. Sort Students Alphabetically:
-SELECT * FROM Student ORDER BY Name ASC;
-
-9. Sort Faculty by Name in Descending Order:
-SELECT * FROM Faculty ORDER BY Name DESC;
-
- 10. Limit Output Rows:
-SELECT * FROM Student LIMIT 2;
-
+-- 6. List staff members and the number of donations they handled (if any)
+SELECT s.Name AS StaffName, COUNT(bd.ID) AS TotalDonations
+FROM Staff s
+LEFT JOIN BloodDonations bd ON s.ID = bd.StaffID
+GROUP BY s.ID
+ORDER BY TotalDonations DESC;
